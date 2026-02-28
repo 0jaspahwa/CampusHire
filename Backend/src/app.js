51e1a76bde9ef1576c1db4f29cd0a8eb416476bc
0jaspahwa <ojaspahwa20@gmail.com>
2026-03-01 05:20:29 +0530

    Update(endPoints)

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,25 @@
 require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const pool = require('./config/db')
 const authRoutes = require('./modules/auth/auth.routes')
-
+const authMiddleware = require('./middleware/auth.middleware')
+const driveRoutes = require('./modules/drives/drive.routes')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-
-
 app.use('/api/auth', authRoutes)
+app.use('/api/drives', driveRoutes)
+
+app.get('/api/protected', authMiddleware, (req, res) => {
+  res.json({
+    message: 'You accessed protected route',
+    user: req.user
+  })
+})
 
 const PORT = process.env.PORT || 5000
 
